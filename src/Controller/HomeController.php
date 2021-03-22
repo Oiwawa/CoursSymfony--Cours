b01d4ab8fb9b524a475891daf58482470446e9e9
Oiwawa <Monstruousbird@Protonmail.com>
2021-03-22 17:23:09 +0100

    Cours 1

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HomeController
+ * @package App\Controller
+ * @Route(path="test/")
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route(path="contact", name="contact")
+     */
+    public function contact(Request $request)
+    {
+
+
+        dump($request->getClientIp());
+
+        $array= ["test", "test", "test"];
+        dump($array);
+
+        dump($request->get('name', 'inconnu'));
+
+        dump($request->get('parametre','valeur default'));
+        exit();
+    }
+
+    /**
+     * @Route(path="about", name="test", methods={"GET","POST"})
+     */
+    public function test(Request $request){
+
+        return new Response("<p>A propos de ! </p>");
+    }
+
+    /**
+     * @Route(path="mentions", name="mentions")
+     */
+    public function mentions(Request $request){
+
+        return $this->redirectToRoute('about');
+    }
+
+
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class UserController
+ * @package App\Controller
+ * @Route(path="user/", name="user_")
+ */
+class UserController extends AbstractController
+{
+
+    /**
+     * @Route(path="register", name="create", methods={"GET","POST"})
+     */
+    public function create() {
+
+        return $this->render('test.html.twig');
+    }
+    /**
+     * @Route(path="login", name="login", methods={"GET","POST"})
+     */
+    public function login() {
+
+        return new Response("<p>Page de connexion </p>");
+    }
+    /**
+     * @Route(path="edit", name="edit", methods={"GET","POST"})
+     */
+    public function edit() {
+
+        return new Response("<p>Page de modification </p>");
+    }
+
+}
